4ccea68539e41148f652762112b315988c1b3f83
meghashree2106 <66686767+meghashree2106@users.noreply.github.com>
2020-07-06 18:12:06 +0530

    Add files via upload

---
+++ files/variables.tf
@@ -0,0 +1,47 @@
+variable "account_id" {
+  type = string
+}
+variable "aws_region" {
+  type = string
+}
+
+variable "ec2_key" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "PublicSubnet1_cidr" {
+  type = string
+}
+
+variable "PublicSubnet2_cidr" {
+  type = string
+}
+
+variable "PrivateSubnet_cidr" {
+  type = string
+}
+
+variable "AZ1" {
+  type = string
+}
+
+variable "AZ2" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "url" {
+  type = string
+}
+
